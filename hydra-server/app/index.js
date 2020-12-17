d2e9b43f984be21fbd44cd1e9c31ec34e1e913d8
Bruno Gola <me@bgo.la>
2020-12-17 10:27:03 +0100

    experimental OSC code receiver

--- hydra-server/app/index.js
+++ hydra-server/app/index.js
@@ -8,11 +8,11 @@ const Menu = require('./src/menu.js')
 const keymaps = require('./keymaps.js')
 const log = require('./src/log.js')
 const repl = require('./src/repl.js')
+const OSC = require("osc")
 
 function init () {
   window.pb = pb
   window.P5 = P5
-
 
   var canvas = document.getElementById('hydra-canvas')
   // canvas.width = window.innerWidth * window.devicePixelRatio
@@ -33,27 +33,27 @@ function init () {
   var pb = new PatchBay()
   var hydra = new HydraSynth({ pb: pb, canvas: canvas, autoLoop: false,  precision: precisionValue})
   var editor = new Editor()
-  var menu = new Menu({ editor: editor, hydra: hydra})
+  //var menu = new Menu({ editor: editor, hydra: hydra})
   log.init()
 
   // get initial code to fill gallery
-  var sketches = new Gallery(function(code, sketchFromURL) {
+  /*var sketches = new Gallery(function(code, sketchFromURL) {
     editor.setValue(code)
     repl.eval(code)
 
     // if a sketch was found based on the URL parameters, dont show intro window
     if(sketchFromURL) {
-      menu.closeModal()
+      //menu.closeModal()
     } else {
-      menu.openModal()
+      //menu.openModal()
     }
   })
-  menu.sketches = sketches
-
+  //menu.sketches = sketches
+  */
   keymaps.init ({
-    editor: editor,
-    gallery: sketches,
-    menu: menu,
+    //editor: editor,
+    //gallery: sketches,
+    //menu: menu,
     repl: repl,
     log: log
   })
@@ -73,6 +73,16 @@ function init () {
   pb.init(hydra.captureStream, {
     server: window.location.origin,
     room: 'iclc'
+  })
+ 
+  var oscPort = new OSC.WebSocketPort({url: "ws://localhost:8765", metadata: true})
+  oscPort.open()
+  oscPort.on("message", function (oscMsg) { 
+      console.log("An OSC message just arrived!"); 
+      if (oscMsg.address == '/code') {
+          var code = oscMsg.args[0].value;
+          repl.eval(code);
+      }; 
   })
 
   var engine = loop(function(dt) {
